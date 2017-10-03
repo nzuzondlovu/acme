5dde87c9772cf71aeca03b61de28feb78272cbfa
Nzuzo R Ndlovu <nzuzondlovu@gmail.com>
2017-10-03 09:30:20 +0200

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AcmeCms::Application.config.secret_token = '5d8cdfe7dfa430f2b04ab08ac36f0e6c6f4532e71eb9de68faa06fe9c6c65b3a9c601594fa783461e8fe33a18bec3dbbbbfbc24fffaaf97c5e03c1c69cc0b223'
